99a6646cc3dc80452b3ad1482fc179208e38a8f2
jorgemasta <jorgems93@gmail.com>
2021-05-11 12:26:44 +0100

    feat: add card with mock data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,41 +1,47 @@
 import React from "react";
 import { Global, css, ThemeProvider } from '@emotion/react'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { theme } from 'unsafe-bc-react-components';
 import emotionReset from 'emotion-reset';
+import { CommerceProvider } from '@bigcommerce/storefront-data-hooks'
+
 
 import Home from "../pages/home";
 import KitchenSink from "../pages/kitchen-sink";
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Global
-        styles={css`
-          @import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;900&family=Red+Hat+Text:wght@400;700&display=swap');
-          ${emotionReset}
-          *, *::after, *::before {
-            box-sizing: border-box;
-            -moz-osx-font-smoothing: grayscale;
-            -webkit-font-smoothing: antialiased;
-          }
-        `}
-      />
-      <Router>
-        <div>
-          <Header></Header>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/kitchen-sink">
-              <KitchenSink />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </ThemeProvider>
-
+    <CommerceProvider locale={'en-US'}>
+      <ThemeProvider theme={theme}>
+        <Global
+          styles={css`
+            @import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;900&family=Red+Hat+Text:wght@400;700&display=swap');
+            ${emotionReset}
+            *, *::after, *::before {
+              box-sizing: border-box;
+              -moz-osx-font-smoothing: grayscale;
+              -webkit-font-smoothing: antialiased;
+            }
+            body {
+              ${theme?.typography?.body as any}
+            }
+          `}
+        />
+        <Router>
+          <div>
+            <Header></Header>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/kitchen-sink">
+                <KitchenSink />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </ThemeProvider>
+    </CommerceProvider>
   );
 }
 
