6e70f781d50a7095b02e2c242c39426f8b71b917
jorgemasta <jorgems93@gmail.com>
2021-05-25 16:43:07 +0100

    fix: remove hardcoded config

--- src/api/graphql.ts
+++ src/api/graphql.ts
@@ -1,8 +1,7 @@
 const config = {
-  commerceUrl: 'https://store-iob9uitsuj.mybigcommerce.com/graphql',
+  commerceUrl: '', // TODO: Get it from `.env`
   // expires 01/14/2021
-  apiToken:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJlYXQiOjE2MTA1NjQzNTYsInN1Yl90eXBlIjoyLCJ0b2tlbl90eXBlIjoxLCJjb3JzIjpbImh0dHA6Ly9sb2NhbGhvc3Q6ODA4MCJdLCJjaWQiOjEsImlhdCI6MTYwNzk3MjM5Mywic3ViIjoiYXRzZnh6eDh2eGdrNndvN2YyMGhnOXJrZ2w5MWs3OCIsInNpZCI6OTk5NzQzNjk5LCJpc3MiOiJCQyJ9.tWxhCESvKtSj6X7i_aQ06s1yX-VPzWg05nS_zCJfnqP5416n8QiI7k1QGvFAeGtHujxcxmnaFtaKVqSgIa2OLA',
+  apiToken: '', // TODO: Get it from `.env`
 }
 
 async function fetchGraphqlApi(
