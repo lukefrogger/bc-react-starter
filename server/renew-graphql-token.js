9b0e4aaa54c97fd49c6b7d20ede3a6ccd2689d26
jorgemasta <jorgems93@gmail.com>
2021-06-07 11:53:19 +0100

    refactor: remove request dependency

--- server/renew-graphql-token.js
+++ server/renew-graphql-token.js
@@ -1,42 +1,42 @@
 import fs from 'fs'
 
-import request from 'request'
+import axios from 'axios'
 
 const [host] = process.env.REACT_APP_API_ENDPOINT.split('/api')
 const expiresAt = parseInt(Date.now() / 1000 + 3600 * 24, 10)
 
-const options = {
+axios('https://api.bigcommerce.com/stores/iob9uitsuj/v3/storefront/api-token', {
   method: 'POST',
-  url: 'https://api.bigcommerce.com/stores/iob9uitsuj/v3/storefront/api-token',
   headers: {
     'content-type': 'application/json',
     'x-auth-token': 'fot3oc8s4odoeblcw975uui6qwafl0x',
   },
-  body: {
+  data: {
     channel_id: 1,
     expires_at: expiresAt,
     allowed_cors_origins: [host],
   },
-  json: true,
-}
+})
+  .then((response) => {
+    const newEnvVar = `BIGCOMMERCE_STOREFRONT_API_TOKEN=${response.data.data.token}`
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error)
-  const newEnvVar = `BIGCOMMERCE_STOREFRONT_API_TOKEN=${body.data.token}`
+    if (process.env.PORT) {
+      throw new Error()
+    }
 
-  if (process.env.PORT) {
-    return
-  }
+    return fs.readFile('.env', 'utf8', (err, data) => {
+      const lines = data.split('\n')
+      const without = lines.filter(
+        (line) =>
+          !/^BIGCOMMERCE_STOREFRONT_API_TOKEN=/.test(line) && line !== ''
+      )
+      const newLines = [...without, newEnvVar].join('\n')
 
-  fs.readFile('.env', 'utf8', (err, data) => {
-    const lines = data.split('\n')
-    const without = lines.filter(
-      (line) => !/^BIGCOMMERCE_STOREFRONT_API_TOKEN=/.test(line) && line !== ''
-    )
-    const newLines = [...without, newEnvVar].join('\n')
-
-    fs.writeFile('.env', newLines, () => {
-      console.log(newLines)
+      fs.writeFile('.env', newLines, () => {
+        console.log(newLines)
+      })
     })
   })
-})
+  .catch((error) => {
+    if (error) throw new Error(error)
+  })
