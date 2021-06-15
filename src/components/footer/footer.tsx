fb6480190710fefb8e4b145c45a8e8ff1df1f3b1
jorgemasta <jorgems93@gmail.com>
2021-06-15 15:32:13 +0100

    feat: add footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,22 +1,125 @@
 import * as React from 'react'
 
+import { Link, LinkProps } from 'react-router-dom'
+
+import { Logo } from '../header/logo'
+import { FooterSocial } from './footer--social'
+import * as styles from './styles'
+
+const FOOTER_ITEMS: {
+  content: string | React.ReactElement
+  key: string
+  to?: LinkProps<unknown>['to']
+}[][] = [
+  [
+    {
+      content: 'Clothing',
+      key: 'clothing',
+      to: '/',
+    },
+    {
+      content: 'Shoes',
+      key: 'shoes',
+
+      to: '/',
+    },
+    {
+      content: 'Accessories',
+      key: 'accessories',
+      to: '/',
+    },
+    {
+      content: 'Trending now',
+      key: 'trending',
+      to: '/',
+    },
+  ],
+  [
+    {
+      content: 'Shipping',
+      key: 'shipping',
+      to: '/',
+    },
+    {
+      content: 'Return',
+      key: 'return',
+      to: '/',
+    },
+    {
+      content: 'Guarantee',
+      key: 'guarantee',
+      to: '/',
+    },
+    {
+      content: 'Privacy policy',
+      key: 'privacy-policy',
+      to: '/',
+    },
+  ],
+  [
+    {
+      content: 'About us',
+      key: 'about-us',
+      to: '/',
+    },
+    {
+      content: 'Help',
+      key: 'help',
+      to: '/',
+    },
+    {
+      content: 'Contact us',
+      key: 'contact-us',
+      to: 'https://mail.google.com/mail/u/0/#inbox',
+    },
+  ],
+  [
+    {
+      key: 'mail-to',
+      content: (
+        <a
+          css={styles.link}
+          href="mailto:info@stellar-shop.io"
+          target="_blank"
+          rel="noreferrer"
+        >
+          info@stellar-shop.io
+        </a>
+      ),
+    },
+    {
+      key: 'address',
+      content: <span>0-800-42-STELLAR</span>,
+    },
+    {
+      key: 'social',
+      content: <FooterSocial />,
+    },
+  ],
+]
+
 export function Footer(): React.ReactElement {
   return (
-    <div>
-      <p
-        style={{
-          backgroundColor: 'gainsboro',
-          width: '100%',
-          height: 80,
-          color: 'white',
-          fontSize: 32,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        Footer
-      </p>
+    <div css={styles.container}>
+      <Logo width="100%" />
+      <div css={styles.wrapper}>
+        {FOOTER_ITEMS.map((items) => (
+          <div css={styles.group} key={items[0].key}>
+            {items.map((item) => {
+              if (!item.to) return item.content
+              return (
+                <Link key={item.key} to={item.to} css={styles.link}>
+                  {item.content}
+                </Link>
+              )
+            })}
+          </div>
+        ))}
+      </div>
+      <div css={styles.rights}>
+        <p>Powered by WordPress</p>
+        <p>© All rights reserved, Modern Tribe</p>
+      </div>
     </div>
   )
 }
---
+++ src/components/footer/styles.ts
@@ -0,0 +1,86 @@
+import { css, SerializedStyles, Theme } from '@emotion/react'
+import { CSSPrimitive } from 'unsafe-bc-react-components/dist/theme/types'
+
+export const container = (theme: Theme): SerializedStyles => css`
+  width: 100%;
+  padding: 48px;
+  background-color: ${theme.colors['neutral-10']};
+`
+
+export const wrapper = (theme: Theme): SerializedStyles => css`
+  display: flex;
+  max-width: 1208px;
+  margin: 0 auto;
+  flex-direction: column;
+  justify-content: center;
+  gap: 40px;
+  padding-top: 40px;
+  text-align: center;
+  ${theme.mq[1]} {
+    flex-flow: wrap;
+    justify-content: space-between;
+    > * {
+      width: calc(50% - 40px);
+    }
+  }
+  ${theme.mq[2]} {
+    text-align: start;
+    > * {
+      align-items: flex-start;
+      width: calc(25% - 40px);
+    }
+  }
+`
+
+export const logo = css`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`
+
+export const group = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
+
+export const link = (theme: Theme): SerializedStyles => css`
+  ${theme.typography.body as CSSPrimitive}
+  color: ${theme.colors['neutral-95']};
+  text-decoration: none;
+  :hover {
+    text-decoration: underline;
+  }
+`
+
+export const social = (theme: Theme): SerializedStyles => css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 48px;
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  ${theme.mq[2]} {
+    margin-top: 24px;
+  }
+`
+
+export const rights = (theme: Theme): SerializedStyles => css`
+  display: flex;
+  padding-top: 48px;
+  gap: 16px;
+  max-width: 1208px;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: baseline;
+  flex-wrap: wrap;
+  text-align: center;
+  ${theme.mq[1]} {
+    padding-top: 72px;
+    justify-content: space-between;
+  }
+`
