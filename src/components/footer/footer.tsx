12a8716cda7237f2b924730d78997bb6451c6ace
jorgemasta <jorgems93@gmail.com>
2021-06-15 15:59:09 +0100

    feat: separate groups

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,118 +3,131 @@ import * as React from 'react'
 import { Link, LinkProps } from 'react-router-dom'
 
 import { Logo } from '../header/logo'
-import { FooterSocial } from './footer--social'
+import { FooterSocial as Social } from './footer--social'
 import * as styles from './styles'
 
-const FOOTER_ITEMS: {
-  content: string | React.ReactElement
+type FooterLink = {
+  label: string
   key: string
-  to?: LinkProps<unknown>['to']
-}[][] = [
-  [
-    {
-      content: 'Clothing',
-      key: 'clothing',
-      to: '/',
-    },
-    {
-      content: 'Shoes',
-      key: 'shoes',
+  to: LinkProps<unknown>['to']
+}
 
-      to: '/',
-    },
-    {
-      content: 'Accessories',
-      key: 'accessories',
-      to: '/',
-    },
-    {
-      content: 'Trending now',
-      key: 'trending',
-      to: '/',
-    },
-  ],
-  [
-    {
-      content: 'Shipping',
-      key: 'shipping',
-      to: '/',
-    },
-    {
-      content: 'Return',
-      key: 'return',
-      to: '/',
-    },
-    {
-      content: 'Guarantee',
-      key: 'guarantee',
-      to: '/',
-    },
-    {
-      content: 'Privacy policy',
-      key: 'privacy-policy',
-      to: '/',
-    },
-  ],
-  [
-    {
-      content: 'About us',
-      key: 'about-us',
-      to: '/',
-    },
-    {
-      content: 'Help',
-      key: 'help',
-      to: '/',
-    },
-    {
-      content: 'Contact us',
-      key: 'contact-us',
-      to: 'https://mail.google.com/mail/u/0/#inbox',
-    },
-  ],
-  [
-    {
-      key: 'mail-to',
-      content: (
-        <a
-          css={styles.link}
-          href="mailto:info@stellar-shop.io"
-          target="_blank"
-          rel="noreferrer"
-        >
-          info@stellar-shop.io
-        </a>
-      ),
-    },
-    {
-      key: 'address',
-      content: <span>0-800-42-STELLAR</span>,
-    },
-    {
-      key: 'social',
-      content: <FooterSocial />,
-    },
-  ],
+const CATEGORIES_PAGES: FooterLink[] = [
+  {
+    label: 'Clothing',
+    key: 'clothing',
+    to: '/',
+  },
+  {
+    label: 'Shoes',
+    key: 'shoes',
+
+    to: '/',
+  },
+  {
+    label: 'Accessories',
+    key: 'accessories',
+    to: '/',
+  },
+  {
+    label: 'Trending now',
+    key: 'trending',
+    to: '/',
+  },
 ]
+
+const LEGAL_PAGES: FooterLink[] = [
+  {
+    label: 'Shipping',
+    key: 'shipping',
+    to: '/',
+  },
+  {
+    label: 'Return',
+    key: 'return',
+    to: '/',
+  },
+  {
+    label: 'Guarantee',
+    key: 'guarantee',
+    to: '/',
+  },
+  {
+    label: 'Privacy policy',
+    key: 'privacy-policy',
+    to: '/',
+  },
+]
+
+const STORE_PAGES: FooterLink[] = [
+  {
+    label: 'About us',
+    key: 'about-us',
+    to: '/',
+  },
+  {
+    label: 'Help',
+    key: 'help',
+    to: '/',
+  },
+  {
+    label: 'Contact us',
+    key: 'contact-us',
+    to: '/',
+  },
+]
+
+const EMAIL = 'info@stellar-shop.io'
+const TELEPHONE = '0-800-42-STELLAR'
 
 export function Footer(): React.ReactElement {
   return (
     <div css={styles.container}>
       <Logo width="100%" />
       <div css={styles.wrapper}>
-        {FOOTER_ITEMS.map((items) => (
-          <div css={styles.group} key={items[0].key}>
-            {items.map((item) => {
-              if (!item.to) return item.content
-              return (
-                <Link key={item.key} to={item.to} css={styles.link}>
-                  {item.content}
-                </Link>
-              )
-            })}
-          </div>
-        ))}
+        <div css={styles.group}>
+          {CATEGORIES_PAGES.map((item) => {
+            return (
+              <Link key={item.key} to={item.to} css={styles.link}>
+                {item.label}
+              </Link>
+            )
+          })}
+        </div>
+        <div css={styles.group}>
+          {LEGAL_PAGES.map((item) => {
+            return (
+              <Link key={item.key} to={item.to} css={styles.link}>
+                {item.label}
+              </Link>
+            )
+          })}
+        </div>
+        <div css={styles.group}>
+          {STORE_PAGES.map((item) => {
+            return (
+              <Link key={item.key} to={item.to} css={styles.link}>
+                {item.label}
+              </Link>
+            )
+          })}
+        </div>
+        <div css={styles.group}>
+          <a
+            css={styles.link}
+            href={`mailto:${EMAIL}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {EMAIL}
+          </a>
+          <span>{TELEPHONE}</span>
+          <Social
+            instagram="https://www.instagram.com/"
+            facebook="https://www.facebook.com/"
+            youtube="https://www.youtube.com/"
+          />
+        </div>
       </div>
       <div css={styles.rights}>
         <p>Powered by WordPress</p>
