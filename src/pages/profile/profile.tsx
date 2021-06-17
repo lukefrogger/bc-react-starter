643114273284b8d13a4f7d7b57cb45fe5f84896a
Václav Zeman <vaclav.zeman@outlook.com>
2021-06-17 16:56:40 +0200

    Use profile form

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,5 +1,24 @@
 import * as React from 'react'
 
+import { useTranslation } from 'react-i18next'
+import { Profile, Typography } from 'unsafe-bc-react-components'
+
+import * as styles from './styles'
+
 export function ProfilePage(): React.ReactElement {
-  return <div>Profile</div>
+  const { t } = useTranslation()
+
+  return (
+    <div css={styles.container}>
+      <Typography css={styles.title} variant="display-large">
+        {t('profile.title', 'Account profile')}
+      </Typography>
+      <Profile.ProfileForm
+        onSubmit={(value) => {
+          console.log(value)
+          return true
+        }}
+      />
+    </div>
+  )
 }
---
+++ src/pages/profile/styles.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react'
+
+export const container = css`
+  padding: 0 20px 120px;
+`
+
+export const title = css`
+  text-align: center;
+  padding: 64px 0;
+`
