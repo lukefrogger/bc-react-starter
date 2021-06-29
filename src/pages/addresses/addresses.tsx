ccd32467c19f5a6f14645fceb04a8e8cb5c46c01
Václav Zeman <vaclav.zeman@outlook.com>
2021-06-29 15:30:52 +0200

    Update UI

--- src/pages/addresses/addresses.tsx
+++ src/pages/addresses/addresses.tsx
@@ -1,18 +1,83 @@
 import * as React from 'react'
 
-import { Profile } from 'unsafe-bc-react-components'
+import useAddresses from '@bigcommerce/storefront-data-hooks/address/use-addresses'
+import { Address } from '@bigcommerce/storefront-data-hooks/api/address'
+import { useTranslation } from 'react-i18next'
+import { useHistory } from 'react-router-dom'
+import { Button, Profile, Typography } from 'unsafe-bc-react-components'
+import { AddressType } from 'unsafe-bc-react-components/dist/components/core/profile/types'
 
-import { useAddresses } from '@hooks/use-addresses'
+const mock: Address[] = [
+  {
+    id: 63,
+    address1: '123 Fake St',
+    address2: '',
+    address_type: 'residential',
+    city: 'Oklahoma',
+    company: '',
+    country: 'United States',
+    country_code: 'US',
+    customer_id: 41,
+    first_name: 'Nick',
+    last_name: 'Ioa',
+    phone: '555678545',
+    postal_code: '123456',
+    state_or_province: 'Florida',
+  },
+  {
+    id: 81,
+    address1: '31',
+    address2: 'Aspin Park Road',
+    address_type: 'residential',
+    city: 'Knaresborough',
+    company: '',
+    country: 'United Kingdom',
+    country_code: 'GB',
+    customer_id: 41,
+    first_name: 'Nick',
+    last_name: 'Ioannou',
+    phone: '',
+    postal_code: 'HG5 8HF',
+    state_or_province: 'North Yorkshire',
+  },
+]
 
 export function AddressesPage(): React.ReactElement {
-  const data = useAddresses({ customerId: 40 })
+  const { t } = useTranslation()
+  const history = useHistory()
+  // TODO: When useCustomer hook works, this should start working too
+  const { data, mutate } = useAddresses()
+  const addresses = data?.addresses || mock
+
+  const handleAdd = (): void => history.push('/user/addresses/add')
+  const handleEdit = (address: AddressType): void => {
+    if (address?.id) history.push(`/user/addresses/edit/${address?.id}`)
+  }
+  const handleDelete = (address: AddressType): void => {
+    mutate({
+      ...data,
+      addresses: addresses.filter((item) => item?.id !== address?.id),
+    } as any)
+  }
 
   return (
     <div>
-      Addresses
-      {/* {addresses?.map((address) => (
-        <Profile.AddressCard key={address.id} address={address} />
-      ))} */}
+      <Typography variant="display-large">
+        {t('addresses.title', 'My addresses')}
+      </Typography>
+      <Button onClick={handleAdd} variant="secondary">
+        {t('addresses.add', 'Add new address')}
+      </Button>
+      {addresses?.map(
+        (address: Address): React.ReactElement => (
+          <Profile.AddressCard
+            key={address.id}
+            address={address as AddressType}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
+        )
+      )}
     </div>
   )
 }
