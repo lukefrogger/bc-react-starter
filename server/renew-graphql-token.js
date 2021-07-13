3c501f825f9ba0858f7189e72840eaf8895c57a5
jorgemasta <jorgems93@gmail.com>
2021-07-13 11:46:18 +0100

    refactor(regen): use env variables

--- server/renew-graphql-token.js
+++ server/renew-graphql-token.js
@@ -5,18 +5,23 @@ import axios from 'axios'
 const [host] = process.env.REACT_APP_API_ENDPOINT.split('/api')
 const expiresAt = parseInt(Date.now() / 1000 + 3600 * 24, 10)
 
-axios('https://api.bigcommerce.com/stores/iob9uitsuj/v3/storefront/api-token', {
-  method: 'POST',
-  headers: {
-    'content-type': 'application/json',
-    'x-auth-token': 'fot3oc8s4odoeblcw975uui6qwafl0x',
-  },
-  data: {
-    channel_id: 1,
-    expires_at: expiresAt,
-    allowed_cors_origins: [host],
-  },
-})
+const { BIGCOMMERCE_STORE_HASH, BIGCOMMERCE_STORE_API_TOKEN } = process.env
+
+axios(
+  `https://api.bigcommerce.com/stores/${BIGCOMMERCE_STORE_HASH}/v3/storefront/api-token`,
+  {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'x-auth-token': BIGCOMMERCE_STORE_API_TOKEN,
+    },
+    data: {
+      channel_id: 1,
+      expires_at: expiresAt,
+      allowed_cors_origins: [host],
+    },
+  }
+)
   .then((response) => {
     const newEnvVar = `BIGCOMMERCE_STOREFRONT_API_TOKEN=${response.data.data.token}`
 
