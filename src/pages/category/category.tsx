1779b6f9730d38c0101a9375fe97b356e8dec385
jorgemasta <jorgems93@gmail.com>
2021-08-12 12:00:44 +0100

    fix: improve navigation between screens

--- src/pages/category/category.tsx
+++ src/pages/category/category.tsx
@@ -160,7 +160,6 @@ export function CategoryPage(): React.ReactElement {
                       url_standard:
                         product.node.images.edges?.[0]?.node.urlOriginal || '',
                     },
-                    productUrl: `/product${product.node.path}`,
                     productId: product.node.entityId,
                     variantId: product.node.variants?.edges?.[0]?.node.entityId, // TODO: Handle variant
                     path: product.node.path,
