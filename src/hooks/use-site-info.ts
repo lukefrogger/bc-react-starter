96ab685a307c1042920d816272e92381a3bf02fa
lukefrogger <lukefrogger@gmail.com>
2022-07-07 12:09:42 -0500

    Formatting

--- src/hooks/use-site-info.ts
+++ src/hooks/use-site-info.ts
@@ -1,13 +1,11 @@
 import { GetSiteInfoResult } from '@bigcommerce/storefront-data-hooks/api/operations/get-site-info'
 import axios from 'axios'
 import useSWR, { SWRResponse } from 'swr'
+
 import { REACT_APP_API_ENDPOINT } from '@config/constants'
 
-
 export const siteInfoFetcher = async (): Promise<any> => {
-  const { data } = await axios(
-    `${REACT_APP_API_ENDPOINT}/api/site-info`
-  )
+  const { data } = await axios(`${REACT_APP_API_ENDPOINT}/api/site-info`)
   return data
 }
 
