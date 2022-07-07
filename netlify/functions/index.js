2a1667833547437893ef80ba030649f5a6ebe794
Jing Wang <jwang@jivewisestudio.com>
2022-07-07 11:40:25 -0400

    fix express import

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,4 +1,5 @@
 const serverless = require('serverless-http')
+const express = require('express');
 const { app, createRoutes } = require('./app')
 
 app.use('/.netlify/functions/index/', createRoutes(express.Router()))
